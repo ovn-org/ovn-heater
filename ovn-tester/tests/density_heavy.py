27d29216da7526bd2e7bdf03216788873afa795d
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2021-07-29 13:23:06 +0200

    density_heavy: add batching support

--- ovn-tester/tests/density_heavy.py
+++ ovn-tester/tests/density_heavy.py
@@ -2,11 +2,13 @@ from collections import namedtuple
 from ovn_context import Context
 from ovn_workload import Namespace
 from ovn_ext_cmd import ExtCmd
+import ovn_exceptions
 
 
 DensityCfg = namedtuple('DensityCfg',
                         ['n_pods',
                          'n_startup',
+                         'batch',
                          'pods_vip_ratio'])
 
 
@@ -18,15 +20,20 @@ class DensityHeavy(ExtCmd):
         self.config = DensityCfg(
             n_pods=test_config.get('n_pods', 0),
             n_startup=test_config.get('n_startup', 0),
+            batch=test_config.get('batch', 1),
             pods_vip_ratio=test_config.get('pods_vip_ratio', 1)
         )
+        if self.config.pods_vip_ratio > self.config.batch or \
+           self.config.batch % self.config.pods_vip_ratio or \
+           self.config.n_pods % self.config.batch:
+            raise ovn_exceptions.OvnInvalidConfigException()
 
     def run(self, ovn, global_cfg):
         if self.config.pods_vip_ratio == 0:
             return
 
         ns = Namespace(ovn, 'ns_density_heavy')
-        with Context('density_heavy_startup', 1, brief_report=True) as ctx:
+        with Context('density_heavy_startup', brief_report=True) as ctx:
             ports = ovn.provision_ports(self.config.n_startup, passive=True)
             ns.add_ports(ports)
             backends = [
@@ -37,11 +44,15 @@ class DensityHeavy(ExtCmd):
 
         with Context('density_heavy',
                      (self.config.n_pods - self.config.n_startup) /
-                     self.config.pods_vip_ratio, test=self) as ctx:
+                     self.config.batch, test=self) as ctx:
             for i in ctx:
-                ports = ovn.provision_ports(self.config.pods_vip_ratio)
+                ports = ovn.provision_ports(self.config.batch)
                 ns.add_ports(ports)
-                ovn.provision_vips_to_load_balancers([[ports[0]]])
+                backends = [
+                        [ports[i]] for i in range(0, self.config.batch,
+                                                  self.config.pods_vip_ratio)
+                ]
+                ovn.provision_vips_to_load_balancers(backends)
                 ovn.ping_ports(ports)
 
         if not global_cfg.cleanup:

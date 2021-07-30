07525c8a4c8d21bc5782bd3250f3dc324f4af9c1
Mark Michelson <mmichels@redhat.com>
2021-07-30 10:13:16 -0400

    ovn-tester: Use QPS method for density_light test.
    Signed-off-by: Mark Michelson <mmichels@redhat.com>

--- ovn-tester/tests/density_light.py
+++ ovn-tester/tests/density_light.py
@@ -7,7 +7,8 @@ from ovn_workload import create_namespace
 DensityCfg = namedtuple('DensityCfg',
                         ['n_pods',
                          'n_startup',
-                         'pods_vip_ratio'])
+                         'pods_vip_ratio',
+                         'queries_per_second'])
 
 
 class DensityLight(ExtCmd):
@@ -18,24 +19,28 @@ class DensityLight(ExtCmd):
         self.config = DensityCfg(
             n_pods=test_config.get('n_pods', 0),
             n_startup=test_config.get('n_startup', 0),
-            pods_vip_ratio=0
+            pods_vip_ratio=0,
+            queries_per_second=config.get('queries_per_second', 20),
         )
 
     async def run(self, ovn, global_cfg):
         ns = await create_namespace(ovn, 'ns_density_light')
-        with Context('density_light_startup', 1, brief_report=True) as ctx:
+        with Context('density_light_startup', brief_report=True) as ctx:
             ports = await ovn.provision_ports(self.config.n_startup,
                                               passive=True)
             await ns.add_ports(ports)
 
         n_iterations = self.config.n_pods - self.config.n_startup
         with Context('density_light', n_iterations, test=self) as ctx:
-            for i in ctx:
-                ports = await ovn.provision_ports(1)
-                await ns.add_ports(ports[0:1])
-                await ovn.ping_ports(ports)
+            await ctx.qps_test(self.config.queries_per_second,
+                               self.provisioner, ns, ovn)
 
         if not global_cfg.cleanup:
             return
         with Context('density_light_cleanup', brief_report=True) as ctx:
             await ns.unprovision()
+
+    async def provisioner(self, ns, ovn):
+        ports = await ovn.provision_ports(1)
+        await ns.add_ports(ports[0:1])
+        await ovn.ping_ports(ports)

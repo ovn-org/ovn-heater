82812525357183331d8159b6bdbdb4f4a2ee6fc0
Mark Michelson <mmichels@redhat.com>
2021-08-03 13:55:06 -0400

    ovn-tester: Use QPS method for netpol_cross_ns test.
    Signed-off-by: Mark Michelson <mmichels@redhat.com>

--- ovn-tester/tests/netpol_cross_ns.py
+++ ovn-tester/tests/netpol_cross_ns.py
@@ -1,11 +1,12 @@
 from collections import namedtuple
-from ovn_context import Context
+from ovn_context import Context, get_current_iteration
 from ovn_ext_cmd import ExtCmd
 from ovn_workload import create_namespace
 
 NpCrossNsCfg = namedtuple('NpCrossNsCfg',
                           ['n_ns',
-                           'pods_ns_ratio'])
+                           'pods_ns_ratio',
+                           'queries_per_second'])
 
 
 class NetpolCrossNs(ExtCmd):
@@ -16,6 +17,7 @@ class NetpolCrossNs(ExtCmd):
         self.config = NpCrossNsCfg(
             n_ns=test_config.get('n_ns', 0),
             pods_ns_ratio=test_config.get('pods_ns_ratio', 0),
+            queries_per_second=test_config.get('queries_per_second', 20),
         )
 
     async def run(self, ovn, global_cfg):
@@ -33,14 +35,18 @@ class NetpolCrossNs(ExtCmd):
                 all_ns.append(ns)
 
         with Context('netpol_cross_ns', self.config.n_ns, test=self) as ctx:
-            for i in ctx:
-                ns = all_ns[i]
-                ext_ns = all_ns[(i+1) % self.config.n_ns]
-                await ns.allow_cross_namespace(ext_ns)
-                await ns.check_enforcing_cross_ns(ext_ns)
+            await ctx.qps_test(self.config.queries_per_second,
+                               self.tester, all_ns)
 
         if not global_cfg.cleanup:
             return
         with Context('netpol_cross_ns_cleanup', brief_report=True) as ctx:
             for ns in all_ns:
                 await ns.unprovision()
+
+    async def tester(self, all_ns):
+        iter_num = get_current_iteration().num
+        ns = all_ns[iter_num]
+        ext_ns = all_ns[(iter_num + 1) % self.config.n_ns]
+        await ns.allow_cross_namespace(ext_ns)
+        await ns.check_enforcing_cross_ns(ext_ns)

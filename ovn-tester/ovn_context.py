e0687716affe514c8bdfeeffbc08cea5cc466de8
Mark Michelson <mmichels@redhat.com>
2021-08-09 08:23:54 -0400

    ovn-tester: Remove unused __iter__ and __next__ from Context.
    With the switch to asynchronous code, these methods are no longer used
    and they're no longer useful. Remove.
    
    Signed-off-by: Mark Michelson <mmichels@redhat.com>

--- ovn-tester/ovn_context.py
+++ ovn-tester/ovn_context.py
@@ -18,8 +18,6 @@ class Context(object):
         self.test_name = test_name
         self.max_iterations = max_iterations
         self.brief_report = brief_report
-        self.iteration_start = None
-        self.failed = False
         self.test = test
         self.iterations = dict()
         if self.max_iterations is None:
@@ -37,32 +35,6 @@ class Context(object):
     def __exit__(self, type, value, traceback):
         ovn_stats.report(self.test_name, brief=self.brief_report)
         log.info(f'Exiting context {self.test_name}')
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        now = time.perf_counter()
-        if self.iteration_start:
-            duration = now - self.iteration_start
-            ovn_stats.add(ITERATION_STAT_NAME, duration, failed=self.failed)
-            log.log(logging.WARNING if self.failed else logging.INFO,
-                    f'Context {self.test_name}, Iteration {self.iteration}, '
-                    f'Result: {"FAILURE" if self.failed else "SUCCESS"}')
-        self.failed = False
-        if self.test:
-            # exec external cmd
-            # self.test.exec_cmd(self.iteration, self.test_name)
-            pass
-        self.iteration_start = now
-        if self.iteration < self.max_iterations - 1:
-            self.iteration += 1
-            log.info(f'Context {self.test_name}, Iteration {self.iteration}')
-            return self.iteration
-        raise StopIteration
-
-    def fail(self):
-        self.failed = True
 
     async def iteration_started(self, iteration):
         '''Explicitly begin iteration 'n'. This is necessary when running

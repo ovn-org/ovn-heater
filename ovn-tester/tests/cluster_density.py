d44847477885fa318a6845ed089fcfcaf1f776e3
Dumitru Ceara <dceara@redhat.com>
2022-10-04 13:49:27 +0200

    cluster_density: Make the startup stage provision the same thing as the main stage.
    Also remove support for batching.  It doesn't make much sense and it's
    not really used.
    
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- ovn-tester/tests/cluster_density.py
+++ ovn-tester/tests/cluster_density.py
@@ -9,10 +9,11 @@ DENSITY_N_BUILD_PODS = 6
 DENSITY_N_PODS = 4
 DENSITY_N_TOT_PODS = DENSITY_N_BUILD_PODS + DENSITY_N_PODS
 
+# In ClusterDensity.run_iteration() we assume at least 4 different pods
+# can be used as backends.
+assert DENSITY_N_PODS >= 4
 
-ClusterDensityCfg = namedtuple(
-    'ClusterDensityCfg', ['n_runs', 'batch', 'n_startup']
-)
+ClusterDensityCfg = namedtuple('ClusterDensityCfg', ['n_runs', 'n_startup'])
 
 
 class ClusterDensity(ExtCmd):
@@ -23,72 +24,54 @@ class ClusterDensity(ExtCmd):
         test_config = config.get('cluster_density', dict())
         self.config = ClusterDensityCfg(
             n_runs=test_config.get('n_runs', 0),
-            batch=test_config.get('batch', DENSITY_N_TOT_PODS),
             n_startup=test_config.get('n_startup', 0),
         )
-        if self.config.batch % DENSITY_N_TOT_PODS:
+        if self.config.n_startup > self.config.n_runs:
             raise ovn_exceptions.OvnInvalidConfigException()
-        self.batch_ratio = self.config.batch // DENSITY_N_TOT_PODS
+
+    def run_iteration(self, ovn, index, global_cfg, passive):
+        ns = Namespace(ovn, f'NS_density_{index}', global_cfg)
+        # Create DENSITY_N_BUILD_PODS short lived "build" pods.
+        build_ports = ovn.provision_ports(DENSITY_N_BUILD_PODS, passive)
+        ns.add_ports(build_ports)
+        if not passive:
+            ovn.ping_ports(build_ports)
+
+        # Add DENSITY_N_PODS test pods and provision them as backends
+        # to the namespace load balancer.
+        ports = ovn.provision_ports(DENSITY_N_PODS, passive)
+        ns.add_ports(ports)
+        ns.create_load_balancer()
+        ovn.provision_lb(ns.load_balancer)
+        ns.provision_vips_to_load_balancers(
+            [ports[0:2], ports[2:3], ports[3:4]]
+        )
+        if not passive:
+            ovn.ping_ports(ports)
+
+        # Remove the short lived pods
+        ns.unprovision_ports(build_ports)
+        return ns
 
     def run(self, ovn, global_cfg):
         all_ns = []
         with Context(ovn, 'cluster_density_startup', brief_report=True) as ctx:
-            # create 4 legacy pods per iteration.
-            ports = ovn.provision_ports(
-                DENSITY_N_PODS * self.config.n_startup, passive=True
-            )
-            for i in range(self.config.n_startup):
-                ns = Namespace(ovn, f'NS_density_{i}', global_cfg)
-                ns.add_ports(
-                    ports[DENSITY_N_PODS * i : DENSITY_N_PODS * (i + 1)]
+            for index in range(self.config.n_startup):
+                all_ns.append(
+                    self.run_iteration(ovn, index, global_cfg, passive=True)
                 )
-                ns.create_load_balancer()
-                ovn.provision_lb(ns.load_balancer)
-                ns.provision_vips_to_load_balancers(
-                    [
-                        ports[i * DENSITY_N_PODS : i * DENSITY_N_PODS + 2],
-                        [ports[i * DENSITY_N_PODS + 2]],
-                        [ports[i * DENSITY_N_PODS + 3]],
-                    ]
-                )
-                all_ns.append(ns)
 
         with Context(
             ovn,
             'cluster_density',
-            (self.config.n_runs - self.config.n_startup) // self.batch_ratio,
+            self.config.n_runs - self.config.n_startup,
             test=self,
         ) as ctx:
             for i in ctx:
-                ns = Namespace(
-                    ovn, f'NS_density_{self.config.n_startup + i}', global_cfg
+                index = self.config.n_startup + i
+                all_ns.append(
+                    self.run_iteration(ovn, index, global_cfg, passive=False)
                 )
-                ns.create_load_balancer()
-                ovn.provision_lb(ns.load_balancer)
-                all_ns.append(ns)
-
-                iter_ports = ovn.provision_ports(self.config.batch)
-                for j in range(self.batch_ratio):
-                    # create 6 short lived "build" pods
-                    build_ports = iter_ports[
-                        j * DENSITY_N_TOT_PODS : j * DENSITY_N_TOT_PODS
-                        + DENSITY_N_BUILD_PODS
-                    ]
-                    ns.add_ports(build_ports)
-                    ovn.ping_ports(build_ports)
-                    # create 4 legacy pods
-                    ports = iter_ports[
-                        j * DENSITY_N_TOT_PODS
-                        + DENSITY_N_BUILD_PODS : (j + 1) * DENSITY_N_TOT_PODS
-                    ]
-                    ns.add_ports(ports)
-                    # add VIPs and backends to namespace load-balancer
-                    ns.provision_vips_to_load_balancers(
-                        [ports[0:2], ports[2:3], ports[3:4]]
-                    )
-
-                    ovn.ping_ports(ports)
-                    ns.unprovision_ports(build_ports)
 
         if not global_cfg.cleanup:
             return

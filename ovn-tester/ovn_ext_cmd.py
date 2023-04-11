8884b1fd41a649b3a1b1dc521085fdfc1fd200f9
Ales Musil <amusil@redhat.com>
2023-04-11 09:48:28 +0200

    ext_cmd: Make the ExtCmd more extensible
    Remove some duplication and use common class for
    configuration. This allows the ExtCmd to be more
    extensible.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- ovn-tester/ovn_ext_cmd.py
+++ ovn-tester/ovn_ext_cmd.py
@@ -1,85 +1,64 @@
-from collections import namedtuple
+from collections import defaultdict
 from io import StringIO
 
-ExtCmdCfg = namedtuple(
-    'ExtCmdCfg',
-    [
-        'iteration',
-        'cmd',
-        'node',
-        'test',
-        'pid_name',
-        'pid_opt',
-        'background_opt',
-    ],
-)
+
+class ExtCmdUnit(object):
+    def __init__(self, conf, central_node, worker_nodes):
+        self.iteration = conf.get('iteration')
+        self.cmd = conf.get('cmd')
+        self.test = conf.get('test')
+        self.pid_name = conf.get('pid_name')
+        self.background_opt = conf.get('background_opt')
+        self.pid_opt = conf.get('pid_opt', '')
+        self.node = None
+
+        node = conf.get('node')
+        if node == central_node.container:
+            self.node = central_node
+            return
+
+        for wn in worker_nodes:
+            if node == wn.container:
+                self.node = wn
+                return
+
+    def is_valid(self):
+        return (
+            self.iteration is not None
+            and self.cmd
+            and self.test
+            and self.node is not None
+        )
+
+    def exec(self):
+        cmd = self.cmd
+
+        if self.pid_name:
+            stdout = StringIO()
+            self.node.run(f'pidof -s {self.pid_name}', stdout=stdout)
+            cmd += f' {self.pid_opt} {stdout.getvalue().strip()}'
+
+        if self.background_opt:
+            cmd += ' &'
+
+        stdout = StringIO()
+        self.node.run(cmd, stdout=stdout)
+        return stdout.getvalue().strip()
 
 
 class ExtCmd(object):
     def __init__(self, config, central_node, worker_nodes):
-        cmd_list = [
-            ExtCmdCfg(
-                iteration=ext_cmd.get('iteration', None),
-                cmd=ext_cmd.get('cmd', ''),
-                node=ext_cmd.get('node', ''),
-                test=ext_cmd.get('test', ''),
-                pid_name=ext_cmd.get('pid_name', ''),
-                pid_opt=ext_cmd.get('pid_opt', ''),
-                background_opt=ext_cmd.get('background_opt', False),
-            )
-            for ext_cmd in config.get('ext_cmd', list())
-        ]
-        cmd_list.sort(key=lambda x: x.iteration)
-        self.cmd_map = self.build_cmd_map(cmd_list, central_node, worker_nodes)
-
-    def build_cmd_map(self, cmd_list, central_node, worker_nodes):
-        cmd_map = dict()
-        for c in cmd_list:
-            if c.iteration is None:
-                continue
-            if not len(c.cmd):
-                continue
-            if not len(c.test):
-                continue
-            if c.node == central_node.container:
-                cmd_map[(c.iteration, c.test)] = {
-                    'node': central_node,
-                    'cmd': c.cmd,
-                    'pid_name': c.pid_name,
-                    'pid_opt': c.pid_opt,
-                    'background_opt': c.background_opt,
-                }
-                continue
-            for w in worker_nodes:
-                if c.node != w.container:
-                    continue
-                cmd_map[(c.iteration, c.test)] = {
-                    'node': w,
-                    'cmd': c.cmd,
-                    'pid_name': c.pid_name,
-                    'pid_opt': c.pid_opt,
-                    'background_opt': c.background_opt,
-                }
-                break
-        return cmd_map
+        self.cmd_map = defaultdict(list)
+        for ext_cmd in config.get('ext_cmd', list()):
+            cmd_unit = ExtCmdUnit(ext_cmd, central_node, worker_nodes)
+            if cmd_unit.is_valid():
+                self.cmd_map[(cmd_unit.iteration, cmd_unit.test)].append(
+                    cmd_unit
+                )
 
     def exec_cmd(self, iteration, test):
-        ext_cmd = self.cmd_map.get((iteration, test), None)
-        if not ext_cmd:
+        ext_cmds = self.cmd_map.get((iteration, test))
+        if not ext_cmds:
             return
 
-        cmd = ext_cmd['cmd']
-        w = ext_cmd['node']
-
-        if len(ext_cmd['pid_name']):
-            stdout = StringIO()
-            w.run("pidof -s " + ext_cmd['pid_name'], stdout=stdout)
-            pid = stdout.getvalue().strip()
-            cmd = cmd + " " + ext_cmd['pid_opt'] + " " + pid
-
-        if ext_cmd['background_opt']:
-            cmd = cmd + " &"
-
-        stdout = StringIO()
-        w.run(cmd, stdout=stdout)
-        return stdout.getvalue().strip()
+        return {ext_cmd: ext_cmd.exec() for ext_cmd in ext_cmds}
